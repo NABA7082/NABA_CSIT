be2788b40ddf4ee648b0f5c7f8535a5a832201a9
NABA7082 <69723668+NABA7082@users.noreply.github.com>
2022-09-11 20:39:46 +0530

    Time: 15 ms (31.20%), Space: 44.8 MB (37.55%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
                 char[] st = s.toCharArray();
         char[] tt = t.toCharArray();
 
         Arrays.sort(st);
         Arrays.sort(tt);
         
-        if(Arrays.equals(st,tt))
-        {
+        //if(Arrays.equals(st,tt))
+        
+       for(int i=0;i<st.length;i++)
+       {
+            
+        /*{
             return true;
         }
         else
         {
             return false;
-        }
+        }*/
+            if(st[i]!=tt[i])
+            {
+                return false;
+            }
+    
             
         
         
     }
+    return true;
+}
 }
